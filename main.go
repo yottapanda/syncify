e67ef378136b3fa59d80422c053b3029d9919fa9
Keval Kapdee <keval@kapdee.uk>
2024-06-20 00:00:14 +0100

    Update security headers

--- main.go
+++ main.go
@@ -57,11 +57,11 @@ func main() {
 			AllowCredentials: true,
 		}),
 		middleware.SetHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' unpkg.com; style-src 'self' 'unsafe-inline'"),
-		middleware.SetHeader("Strict-Transport-Security", "max-age=300"),
+		middleware.SetHeader("Strict-Transport-Security", "max-age=2592000"),
 		middleware.SetHeader("X-Frame-Options", "DENY"),
 		middleware.SetHeader("X-Content-Type-Options", "nosniff"),
 		middleware.SetHeader("Referrer-Policy", "strict-origin"),
-		middleware.SetHeader("Permissions-Policy", ""),
+		middleware.SetHeader("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), clipboard-read=(), clipboard-write=(), gamepad=(), speaker-selection=(), conversion-measurement=(), focus-without-user-activation=(), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), trust-token-redemption=(), unload=(), window-placement=(), vertical-scroll=()"),
 	)
 
 	r.Route("/", func(r chi.Router) {
