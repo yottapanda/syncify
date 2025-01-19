c2660be29b5a803e55711f6cd44f259f38848950
Keval Kapdee <keval@kapdee.uk>
2025-01-19 12:28:13 +0000

    Spotipy login and logout (#3)
    * Spotipy login and logout
    
    * Example .env
    
    * dotenv package

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+
+import spotipy
+from flask import Flask, redirect, request, session
+
+from dotenv import load_dotenv
+from flask_session import Session
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("SECRET_KEY")
+
+Session(app)
+
+scope = "playlist-read-private,playlist-modify-private,user-library-read,playlist-modify-public"
+
+@app.route("/auth/login")
+def login():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    auth_url = auth_manager.get_authorize_url()
+
+    return redirect(auth_url)
+
+@app.route("/auth/callback")
+def callback():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+
+    session['token'] = auth_manager.get_access_token(request.args.get('code'))['access_token']
+
+    return redirect('/')
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route("/")
+def home():
+    if 'token' not in session:
+        return redirect('/auth/login')
+    spotify = spotipy.Spotify(auth=session['token'])
+    user = spotify.current_user()
+    return f"""<html>
+    <body>
+    <h1>Syncify 2!</h1>
+    <p>Logged in as {user['display_name']}</p>
+    <a href="/sync">Sync</a>
+    <a href="/logout">Logout</a>
+    </body>
+    </html>"""
+
+@app.route("/sync", methods=["POST"])
+def sync():
+    return "Syncing... not implemented yet"
