920cab35037cd3f7e6e174aa28caf5d983894c0d
Paul Baker <112940979+Paulbaker17@users.noreply.github.com>
2025-01-19 22:16:32 +0000

    Sync function (#11)
    * Sync takes in all liked songs into a list
    
    * Added pycache
    
    * sync function
    
    * Update webapp/app.py
    
    Co-authored-by: Keval Kapdee <keval@kapdee.uk>
    
    * Update webapp/app.py
    
    Co-authored-by: Keval Kapdee <keval@kapdee.uk>
    
    * sync function
    
    * sync functional but has an upper limit on playlist size
    
    * fixed reccuring fiftieth song and optimised sync func
    
    * added exception handling
    
    * spacing         or something
    
    * order done
    
    ---------
    
    Co-authored-by: Keval Kapdee <keval@kapdee.uk>

--- webapp/app.py
+++ webapp/app.py
@@ -126,6 +126,39 @@ def home():
     </html>"""
 
 
+def get_liked_track_uris(spotify):
+    results = spotify.current_user_saved_tracks(limit=50, offset=0)
+
+    track_uris = []
+    for track in results['items']:
+        track_uris.append(track['track']['uri'])
+
+    while results['next']:
+        results = spotify.next(results)
+        for track in results['items']:
+            track_uris.append(track['track']['uri'])
+
+    return track_uris
+
+
+def get_playlist_id(spotify):
+    results = spotify.current_user_playlists(limit=50, offset=0)
+
+    for playlist in results['items']:
+        if playlist['name'] == "Syncify (Liked Songs)":
+            return playlist['id']
+
+    while results['next']:
+        results = spotify.next(results)
+        for playlist in results['items']:
+            if playlist['name'] == "Syncify (Liked Songs)":
+                return playlist['id']
+
+    user = spotify.current_user()
+    playlist = spotify.user_playlist_create(user['id'], "Syncify (Liked Songs)", public=False)
+    return playlist['id']
+
+
 @app.route("/sync")
 def sync():
     if 'id' not in session:
@@ -136,16 +169,19 @@ def sync():
         return redirect('/')
 
     spotify = spotipy.Spotify(auth=user['access_token'])
-    results = spotify.current_user_saved_tracks(limit=50, offset=0)
-
-    track_ids = []
-
-    for track in results['items']:
-        track_ids.append(track['track']['id'])
-
-    while results['next']:
-        results = spotify.next(results)
-        for track in results['items']:
-            track_ids.append(track['track']['id'])
     
-    return track_ids
+    track_uris = get_liked_track_uris(spotify)
+    playlist_id = get_playlist_id(spotify)
+    
+    spotify.playlist_replace_items(playlist_id, [])
+
+    chunks = [track_uris[i:i + 50] for i in range(0, len(track_uris), 50)]
+    
+    for chunk in chunks:
+        try:
+            spotify.playlist_add_items(playlist_id, chunk)
+        except Exception as e:
+            #TODO Handle playlist sizing issue
+            print (e)
+
+    return spotify.playlist(playlist_id)
